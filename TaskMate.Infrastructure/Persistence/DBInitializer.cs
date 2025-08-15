b2fc0da613d3945806148f8f3952aa9b7082ec38
ihanafy <mostafaelhanfy51@gmail.com>
2025-08-15 21:29:42 +0300

    Created Db Initializer

---
+++ TaskMate.Infrastructure/Persistence/DBInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TaskMate.Infrastructure.Persistence
+{
+    public class DBInitializer
+    {
+        private readonly ILogger<DBInitializer> _logger;
+
+        public DBInitializer(ILogger<DBInitializer> logger)
+        {
+            _logger = logger;
+        }
+
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<AppDbContext>();
+
+            if ((await context.Database.GetPendingMigrationsAsync()).Count() > 0)
+                await context.Database.MigrateAsync();
+        }
+    }
+}
